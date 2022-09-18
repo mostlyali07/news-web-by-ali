b406d908b5f596e4a7b9e5df304fda519b481384
Syed Ali Shah <104094853+mostlyali07@users.noreply.github.com>
2022-09-18 05:03:38 +0500

    update

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-fetch = `https://newsapi.org/v2/everything?q=keyword&apiKey=91f47267d3b94d4d8e6cec6fca0ed5d8`;
+fetch(`https://newsapi.org/v2/everything?q=keyword&apiKey=91f47267d3b94d4d8e6cec6fca0ed5d8`)
+.then((name) => name.json())
+.then((name) => {
+    daTa(name)
+}).catch((err) => {
+    console.log(err)
+})
+
+function daTa(name) {
+    let breaKingNews = document.querySelector('breaking-news');
+    breaKingNews += `<h1>${name.title}</h1>`;
+}
+daTa(name);

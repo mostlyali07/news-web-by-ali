d98ecace90e66fd51f0466e70841bcf422c61ad3
mostlyali07 <104094853+mostlyali07@users.noreply.github.com>
2023-01-06 22:46:19 +0500

    Update

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const news = document.getElementById("breaking-news");
 fetch('https://newsapi.org/v2/everything?q=karachi&apiKey=91f47267d3b94d4d8e6cec6fca0ed5d8')
   .then((res) => res.json())
-  .then((json) => {
-    news.innerHTML = `<h2>${json.title}</h2>`;
+  .then((res) => {
+    heading_one.innerHTML = `${res.articles[0].title}`;
+    description_one.innerHTML = `${res.articles[0].description}`;
   });

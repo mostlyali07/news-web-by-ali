9d1a56d6264c8031094f80b3352e5639c9bc95ee
mostlyali07 <104094853+mostlyali07@users.noreply.github.com>
2023-01-09 20:47:09 +0500

    Update

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ fetch(
     Source.innerHTML = `<a href="${sourceurl}" class="btn btn-dark" target="_blank" >View Source</a>`;
     Published.innerHTML = `<b>Published At : ${res.articles[0].publishedAt}</b>`;
 
-
     heading_two.innerHTML = `${res.articles[1].title}`;
     description_two.innerHTML = `${res.articles[1].description}`;
     const admin_2 = res.articles[1].urlToImage;
@@ -19,4 +18,20 @@ fetch(
     const sourceurl_2 = res.articles[1].url;
     Source_2.innerHTML = `<a href="${sourceurl_2}" class="btn btn-dark" target="_blank" >View Source</a>`;
     Published_2.innerHTML = `<b>Published At : ${res.articles[1].publishedAt}</b>`;
+
+    heading_three.innerHTML = `${res.articles[2].title}`;
+    description_three.innerHTML = `${res.articles[2].description}`;
+    const admin_3 = res.articles[2].urlToImage;
+    news_img_three.innerHTML = `<img src="${admin_3}"/>`;
+    const sourceurl_3 = res.articles[2].url;
+    Source_3.innerHTML = `<a href="${sourceurl_3}" class="btn btn-dark" target="_blank" >View Source</a>`;
+    Published_3.innerHTML = `<b>Published At : ${res.articles[2].publishedAt}</b>`;
+
+    heading_four.innerHTML = `${res.articles[3].title}`;
+    description_four.innerHTML = `${res.articles[3].description}`;
+    const admin_4 = res.articles[3].urlToImage;
+    news_img_four.innerHTML = `<img src="${admin_4}"/>`;
+    const sourceurl_4 = res.articles[3].url;
+    Source_4.innerHTML = `<a href="${sourceurl_4}" class="btn btn-dark" target="_blank" >View Source</a>`;
+    Published_4.innerHTML = `<b>Published At : ${res.articles[3].publishedAt}</b>`;
   });
